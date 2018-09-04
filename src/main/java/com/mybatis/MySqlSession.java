709a1ffff6d2f0177d1eea3429c895cc0101eb51
lelling <1156677011@qq.com>
2018-09-04 15:17:46 +0800

    t

--- src/main/java/com/mybatis/MySqlSession.java
+++ src/main/java/com/mybatis/MySqlSession.java
@@ -1,5 +1,6 @@
 package com.mybatis;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
@@ -19,25 +20,30 @@ public class MySqlSession {
 	private static SqlSessionFactoryBuilder sqlSessionFactoryBuilder;
 	
 	private static SqlSessionFactory sqlSessionFactory;
+	static{
+		String resource = "mybatis/mybatis-config.xml";
+		Reader reader = null;
+		try {
+			reader = Resources.getResourceAsReader(resource);
+			sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+			sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
+		} catch (IOException e) {
+			System.err.println(">>>>>>>>>>>>>>异常>>>>>>>>>>>>");
+		}
+	}
 	
 	public static void main(String... args) throws Exception{
-		
-		String resource = "mybatis/mybatis-config.xml";
-		Reader reader = Resources.getResourceAsReader(resource);
-		sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-		sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
 		SqlSession sqlSession = sqlSessionFactory.openSession();
-		
 		MyInterface mapper = sqlSession.getMapper(MyInterface.class);
 
 		/**
-		 * 单条查询，做后置合并行
+		 * 1-单条查询，做后置合并行
 		 */
 		Orders order = mapper.qryOrderById(1);
 		System.out.println(new Gson().toJson(order));
 		
 		/**
-		 * 分页查询
+		 * 2-分页查询
 		 * 此时total为所有关联到的结果数，返回列表时会合并list类型属性，导致当前分页数据个数减少
 		 */
 		Page page = PageHelper.startPage(1, 10, true, true, null);
@@ -45,5 +51,7 @@ public class MySqlSession {
 		int total = (int) page.getTotal();
 		System.out.println( orders.size() + ", " + page.getTotal() + "---"+ new Gson().toJson(orders));
 		
+		
+		sqlSession.close();
 	}
 }
