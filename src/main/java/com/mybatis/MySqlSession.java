e9329428e9bab62d4ee0bdc91d8bc6ad91e19d82
lelling <1156677011@qq.com>
2018-09-04 15:27:18 +0800

    t

--- src/main/java/com/mybatis/MySqlSession.java
+++ src/main/java/com/mybatis/MySqlSession.java
@@ -14,6 +14,7 @@ import com.github.pagehelper.PageHelper;
 import com.google.gson.Gson;
 import com.mybatis.mapper.MyInterface;
 import com.mybatis.model.Orders;
+import com.mybatis.utils.SqlHelper;
 
 public class MySqlSession {
 	
@@ -40,6 +41,8 @@ public class MySqlSession {
 		 * 1-单条查询，做后置合并行
 		 */
 		Orders order = mapper.qryOrderById(1);
+		String sql = SqlHelper.getMapperSql(sqlSession, MyInterface.class, "qryOrderById", new Object[]{2});
+		System.out.println(sql);
 		System.out.println(new Gson().toJson(order));
 		
 		/**
