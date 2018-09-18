bc508ecb1183955bb9aa992ca9d7cb992bb9064c
lelling <1156677011@qq.com>
2018-09-18 11:15:20 +0800

    修改

--- src/main/java/com/mybatis/MySqlSession.java
+++ src/main/java/com/mybatis/MySqlSession.java
@@ -2,15 +2,12 @@ package com.mybatis;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
 import com.google.gson.Gson;
 import com.mybatis.mapper.MyInterface;
 import com.mybatis.model.Orders;
@@ -37,14 +34,15 @@ public class MySqlSession {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		MyInterface mapper = sqlSession.getMapper(MyInterface.class);
 		
-		Orders orderOne = sqlSession.selectOne("com.mybatis.mapper.MyInterface.qryOrderById", 1);
+		Orders orderOne = sqlSession.selectOne("com.mybatis.mapper.MyInterface.qryOrderById", 3);
 		System.out.println(new Gson().toJson(orderOne));
 		
 		/**
 		 * 1-单条查询，做后置合并行
 		 */
-		Orders order = mapper.qryOrderById(1);
-		String sql = SqlHelper.getMapperSql(sqlSession, MyInterface.class, "qryOrderById", new Object[]{2});
+		int orderId = 2;
+		Orders order = mapper.qryOrderById(orderId);
+		String sql = SqlHelper.getMapperSql(sqlSession, MyInterface.class, "qryOrderById", new Object[]{orderId});
 		System.out.println(sql);
 		System.out.println(new Gson().toJson(order));
 		
@@ -52,12 +50,10 @@ public class MySqlSession {
 		 * 2-分页查询
 		 * 此时total为所有关联到的结果数，返回列表时会合并list类型属性，导致当前分页数据个数减少
 		 */
-		Page page = PageHelper.startPage(1, 10, true, true, null);
-		List<Orders> orders = mapper.qryOrderForList(null);
-		int total = (int) page.getTotal();
-		System.out.println( orders.size() + ", " + page.getTotal() + "---"+ new Gson().toJson(orders));
-		
-		
+//		Page page = PageHelper.startPage(1, 10, true, true, null);
+//		List<Orders> orders = mapper.qryOrderForList(null);
+//		int total = (int) page.getTotal();
+//		System.out.println( orders.size() + ", " + page.getTotal() + "---"+ new Gson().toJson(orders));
 		
 		sqlSession.close();
 	}
